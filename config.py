6c760439a6d29d3134cd702a87916c955a6dcb47
python <exit>
2015-08-03 10:06:49 -0700

    Added deployment function, changed some config variables

--- config.py
+++ config.py
@@ -2,7 +2,15 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "This is a terrible secret key!"
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SUBJECT_PREFIX = '[KaplanEverAfter] '
+    MAIL_SENDER = 'Gina And Wes <Wina@KaplanEverAfter.com>'
 
     @staticmethod
     def init_app(app):
@@ -13,13 +21,6 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_SUBJECT_PREFIX = '[KaplanEverAfter]'
-    MAIL_SENDER = 'Gina And Wes <Wina@KaplanEverAfter.com>'
     SERVER_NAME = 'localhost:5000'
 
 class ProductionConfig(Config):
