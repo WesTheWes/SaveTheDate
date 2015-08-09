27b32535ef11c220305582e51edb4d9024b135c6
python <exit>
2015-08-09 05:08:20 -0700

    added tornado web server

--- manage.py
+++ manage.py
@@ -20,6 +20,15 @@ def deploy():
     from flask.ext.migrate import upgrade
     upgrade()
 
+@manager.command
+def run():
+    """Run Tornado Server"""
+    from tornado.wsgi import WSGIContainer
+    from tornado.httpserver import HTTPServer
+    from tornado.ioloop import IOLoop
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(os.getenv('PORT', 5000))
+    IOLoop.instance().start()
 
 if __name__ == '__main__':
     manager.run()
